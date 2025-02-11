ce5e34b75248be59d130f7608aa872fbd2e96f73
Eduardo-Barros08 <edubarsilva29@gmail.com>
2025-02-11 11:54:54 -0400

    exclusão de "MinhaClasse.class"

---
+++ JavaTop/JavaCodes/primeiros códigos Java/java-anatomia-classes/src/edu/barros/primeiraSemanadeEstudos/PrimeiroCodigoJava.java
@@ -0,0 +1,8 @@
+package edu.barros.primeiraSemanadeEstudos;
+public class PrimeiroCodigoJava {
+    
+    public static void main(String[] args) {
+        System.out.println("Esse é meu primeiro código em JAVA");
+    }
+    
+}
